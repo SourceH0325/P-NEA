991a2984065393d107f4b93ded50b7106914e50f
SourceH <zizark4040@gmail.com>
2022-08-10 18:42:33 +0900

    디자인 업데이트

--- src/App.js
+++ src/App.js
@@ -19,13 +19,14 @@ function App() {
         <p className="text-[90px]">D-{result}</p>
       </div>
       <div className="mt-20 text-center">
-        <button onClick={onIncrease} className="hover: rounded-md bg-neutral-600 pr-3 pl-3 text-[24px] text-[#212529] hover:bg-[#0051ff]">응원 추가</button>
+        <button onClick={onIncrease} className="transition duration-300 ease-in-out rounded-md bg-neutral-600 pr-3 pl-3 text-[24px] text-[#212529] hover:bg-[#0051ff]">응원 추가</button>
       </div>
       <div className="mt-20 text-center">
         <button className="rounded-md bg-orange-500 pr-3 pl-3 text-[24px] text-white" disabled>응원 수 : {Count}</button>
       </div>
       <div className="absolute bottom-0 left-0 right-0 mb-2 mt-2 text-center">
         <p className="text-neutral-600">(ง •̀ω•́)ง 화이팅!</p>
+        <a className="transition duration-300 ease-in-out text-neutral-600 underline underline-offset-4 hover:text-white" href="https://github.com/SourceH0325/Future">GITHUB</a>
       </div>
     </div>
   )
